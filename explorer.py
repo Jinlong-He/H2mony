a89e9765fe0acdec161583fb5072b0969f393fe7
hbr <441748128@qq.com>
2024-12-26 18:15:00 +0800

    modify explorer.py hstg.py

--- explorer.py
+++ explorer.py
@@ -20,22 +20,34 @@ class Explorer(object):
         if not service_list:
             return
         if not depth:
+            hstg.del_event()
             return
         u2 = self.u2
         elements = u2(clickable='true')
-        # todo elements order
         if not elements:
+            hstg.del_event()
             return
+        # todo elements order filter
+        package_name = self.package_name
         for element in elements:
             element.click()
             time.sleep(1)
-            for service in service_list:
-                if self.adb.get_audio_status(self.package_name) in ['START', 'START*', 'DUCK']:
-                    service_list.remove(service)
-                    hstg.add_state(service)
-                    print(hstg.states[0].act_name, hstg.states[0].audio_status)
-                    self.explore_dfs(depth-1, hstg, service_list)
-            # todo 回退上一步
+            hstg.add_event(element)
+            audio_status = self.adb.get_audio_status(package_name)
+            if audio_status:
+                flag = False
+                keys = [key for key, value in audio_status.items() if key in service_list and value in ['START', 'START*', 'DUCK']]
+                if keys:
+                    flag = True
+                    service_list = service_list - keys
+                # service start
+                if flag and True in hstg.add_state():
+                    print(f"add state: {hstg.now_state.act_name}, {hstg.now_state.audio_status}")
+                    hstg.add_edge()
+            self.explore_dfs(depth-1, hstg, service_list)
+            hstg.del_event(element)
+            # back to bef state
+            hstg.back()
         return
 
     def explore_bfs(self, hstg, service_list=[]):
@@ -74,18 +86,21 @@ class Explorer(object):
                 element.click()
                 time.sleep(1)
                 for service in services:
+                    print(self.adb.get_audio_status(self.package_name))
+                    return
                     # todo check start start* duck
-                    if self.adb.get_audio_status(self.package_name, service) in ['START', 'START*', 'DUCK']:
-                        services.remove(service)
-                        hstg.add_state(service)
-                        # todo hstg.add_edge()
-                        print(hstg.states[0].act_name, hstg.states[0].audio_status)
-                        if not services:
-                            return
+                    # if self.adb.get_audio_status(self.package_name, service) in ['START', 'START*', 'DUCK']:
+                        # services.remove(service)
+                        # hstg.add_state(service)
+                        # # todo hstg.add_edge()
+                        # print(hstg.states[0].act_name, hstg.states[0].audio_status)
+                        # if not services:
+                        #     return
                 # todo 回退上一步
         return
 
     def test_explore_for_audio(self, hstg, service_list=[]):
-        xml_hierarchy = self.u2.dump_hierarchy()
-        with open(f'xml_hierarchy.xml', 'w', encoding='utf-8') as f:
-            f.write(xml_hierarchy)
+        # xml_hierarchy = self.u2.dump_hierarchy()
+        # with open(f'xml_hierarchy.xml', 'w', encoding='utf-8') as f:
+        #     f.write(xml_hierarchy)
+        pass
--- hstg.py
+++ hstg.py
@@ -1,9 +1,33 @@
 class State(object):
-    def __init__(self, act_name, service_name, audio_status):
+    def __init__(self, act_name, audio_status):
         self.act_name = act_name
-        self.service_name = service_name
+        # audio_status: key=service_name value=status
         self.audio_status = audio_status
-        # todo key=service_name value=status
+
+
+class Edge(object):
+    def __init__(self, source_state, target_state, events):
+        self.source_state = source_state
+        self.target_state = target_state
+        self.events = events
+
+
+class Event(object):
+    def __init__(self, elem):
+        # pos
+        self.elem = elem
+        info = elem.get('info')
+        if not info:
+            assert False
+        bound = info.get('bound')
+        if not bound:
+            assert False
+        top = bound.get('top')
+        bottom = bound.get('bottom')
+        left = bound.get('left')
+        right = bound.get('right')
+        self.posx = int((left+right)/2)
+        self.posy = int((top+bottom)/2)
 
 
 class HSTG(object):
@@ -11,20 +35,55 @@ class HSTG(object):
         # self.app = app
         self.device = device
         self.states = []
-        # todo s0
+        self.edges = []
+        self.events = []
+        self.visit_states = []
+        self.now_state = None
+        self.bef_state = None
+        self.add_state()
 
     # todo def try_back(self, current_state, pre_state):
-    # todo fail restart
-
-    def add_state(self, service_name):
+    def back(self):
+        self.device.u2.back()
+        # todo
+        self.visit_states.pop()
+        check_state = self.visit_states[-1]
         package_name = self.device.adb.get_current_package()
         act_name = self.device.adb.get_current_activity()
-        audio_status = self.device.adb.get_audio_status(package_name, service_name)
-        state = State(act_name, service_name, audio_status)
-        # todo state isequal
-        # todo return (state flag)
-        self.states.append(state)
-        return state
+        audio_status = self.device.adb.get_audio_status(package_name)
+        state = State(act_name, audio_status)
+        if state == check_state:
+            return
+        self.go_state()
 
-    def add_edge(self, source, target, events):
+    def go_state(self):
         pass
+
+    def add_state(self):
+        package_name = self.device.adb.get_current_package()
+        act_name = self.device.adb.get_current_activity()
+        audio_status = self.device.adb.get_audio_status(package_name)
+        state = State(act_name, audio_status)
+        if state in self.states:
+            # todo bef_state now_state change or no change
+            return state, False
+        if len(self.states):
+            self.bef_state = self.states[-1]
+        self.states.append(state)
+        self.visit_states.append(state)
+        self.now_state = self.states[-1]
+        return state, True
+
+    def add_edge(self):
+        # source, target, events
+        edge = Edge(self.bef_state, self.now_state, self.events)
+        self.edges.append(edge)
+        self.events = []
+
+    def add_event(self, elem):
+        event = Event(elem)
+        self.events.append(event)
+
+    def del_event(self):
+        if len(self.events):
+            self.events.pop()
